bb8b578b57a84dd97d92376d442a27c43339a6f2
birinder singh <birinder229.be22@chitkara.edu.in>
2025-01-18 04:20:26 +0530

    basic frontend completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,38 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Services from "./components/Services.jsx";
+import Banner from "./components/Banner.jsx";
+import AppStore from "./components/AppStore.jsx";
+import CoverBanner from "./components/CoverBanner.jsx";
+import Testimonial from "./components/Testimonial.jsx";
+import Footer from "./components/Footer.jsx";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const App = () => {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 500,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
   return (
-    <>
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
       <Navbar />
       <Hero />
-    </>
-  )
-}
+      <Services />
+      <Banner />
+      {/* <CoverBanner /> */}
+      <AppStore />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
+};
 
-export default App
+export default App;
